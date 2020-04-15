1ddc54b6a0594cda1e98df31b899e219ddca59a2
saung3754 <saung3754@nntuple.com>
2020-04-15 22:24:59 +0900

    sectionWrap 함수 정리

--- src/components/section/SectionWrap.js
+++ src/components/section/SectionWrap.js
@@ -42,6 +42,48 @@ class SectionWrap extends Component {
       </ul>
     );
   };
+  
+  nowplay =()=>{
+    const nowplay = this.props.nowData.data.results;
+    const genreData = this.props.genreData.data.genres;
+    return(
+      <div className="section-current-roll">
+            {nowplay.map((info, index) => {
+              return (
+                <div key={index}>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500/${info.poster_path}`}
+                    alt=""
+                  />
+                  <div className="upcoming-box">
+                    <div>
+                      <p className="title">{info.title}</p>
+                      <p className="data-count">
+                        <span>{info.release_date}</span> ⭐️
+                        {info.vote_average} / 10
+                      </p>
+                      <div>
+                        <p className="genre">
+                          {genreData.map((genre) => {
+                            let genreName = "";
+
+                            if (genre.id === info.genre_ids[0]) {
+                              genreName = genre.name;
+                            }
+
+                            return genreName;
+                          })}
+                        </p>
+                        <p className="subTitle">{info.overview}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+    );
+  }
 
   upcome = () => {
     const upcomes = this.props.upcomeData.data.results;
@@ -86,54 +128,20 @@ class SectionWrap extends Component {
     );
   };
   render() {
-    const nowplay = this.props.nowData.data.results;
-    const genreData = this.props.genreData.data.genres;
-
     return (
       <section className="section-wrap">
         <div className="section-main_roll">{this.mainRoll()}</div>
+        {/* main-roll */}
         <div className="section-current-movies">
           <p>현재상영작</p>
-          <div className="section-current-roll">
-            {nowplay.map((info, index) => {
-              return (
-                <div key={index}>
-                  <img
-                    src={`https://image.tmdb.org/t/p/w500/${info.poster_path}`}
-                    alt=""
-                  />
-                  <div className="upcoming-box">
-                    <div>
-                      <p className="title">{info.title}</p>
-                      <p className="data-count">
-                        <span>{info.release_date}</span> ⭐️
-                        {info.vote_average} / 10
-                      </p>
-                      <div>
-                        <p className="genre">
-                          {genreData.map((genre) => {
-                            let genreName = "";
-
-                            if (genre.id === info.genre_ids[0]) {
-                              genreName = genre.name;
-                            }
-
-                            return genreName;
-                          })}
-                        </p>
-                        <p className="subTitle">{info.overview}</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
+          {this.nowplay()}
         </div>
+        {/* 현재 상영작 */}
         <div className="section-schedule-movies">
           <p>개봉예정작</p>
           {this.upcome()}
         </div>
+        {/* 개봉 예정작 */}
       </section>
     );
   }
