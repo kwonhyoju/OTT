62b30d259932351b0c4dee07d373a666322f62e3
saung3754 <saung3754@nntuple.com>
2020-04-15 22:20:58 +0900

    container error예외처리

---
+++ src/components/Error.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Error extends React.Component{
+    render(){
+    return<div className="error">일시적오류 입니다.</div>;
+    }
+}
+
+export default Error;
--- src/components/container/SectionContainer.js
+++ src/components/container/SectionContainer.js
@@ -2,33 +2,51 @@ import React, { Component, Fragment } from "react";
 import { inject, observer } from "mobx-react";
 import SectionWrap from "../section/SectionWrap";
 import Loading from "../Loading";
+import Error from "../Error";
 
 @inject("apiStore")
 @observer
 class SectionContainer extends Component {
-    componentDidMount() {
-        this.props.apiStore.popularData();
-        this.props.apiStore.nowpalyData();
-        this.props.apiStore.upcomingData();
-        this.props.apiStore.getGenre();
+    constructor(props){
+        super(props);
+        this.state={
+            error:null
+        }
+    }
+
+    async componentDidMount() {
+        try{
+            this.props.apiStore.popularData();
+            this.props.apiStore.nowpalyData();
+            this.props.apiStore.upcomingData();
+            this.props.apiStore.getGenre();
+        }catch(error){
+            this.setState({
+                error
+            })
+        }
     }
 
     render() {
         const loading = this.props.apiStore.isLoading;
-        return (
-            <Fragment>
-                {loading ? (
-                    <SectionWrap
-                        popData={this.props.apiStore.popData}
-                        nowData={this.props.apiStore.nowData}
-                        upcomeData={this.props.apiStore.upcomeData}
-                        genreData={this.props.apiStore.genreData}
-                    />
-                ) : (
-                    <Loading />
-                )}
+        if(this.state.error === null){
+            return  (
+                <Fragment>
+                    {loading ? (
+                        <SectionWrap
+                            popData={this.props.apiStore.popData}
+                            nowData={this.props.apiStore.nowData}
+                            upcomeData={this.props.apiStore.upcomeData}
+                            genreData={this.props.apiStore.genreData}
+                        />
+                    ) : (
+                        <Loading />
+                    )} 
             </Fragment>
-        );
+            );
+        }else{
+           return <Error/>
+        }
     }
 }
 
