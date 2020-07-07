77d8a5f1e30f43a1041a7c5e1a960ec3db968fd7
권효주 <saung3754@nntuple.com>
2020-07-07 18:36:16 +0900

    with Boxoffice 생성 , router 설정

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,17 +5,21 @@ import SectionContainer from "./container/SectionContainer";
 
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
+import withBoxoffice from "../hoc/withBoxoffice";
 
 export default () => (
-  <HashRouter>
-    <>
-      <Header />
-      <Switch>
-        <Route path="/" exact component={SectionContainer} />
-        <Route path="/movie/:id" component={DetailContainer} />
-        <Redirect from="*" to="/" />
-      </Switch>
-      <Footer />
-    </>
-  </HashRouter>
+    // const url =
+    //     "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.xml?key=c7681d4ec8dd225970ea706ab&targetDt=20120101";
+    <HashRouter>
+        <>
+            <Header />
+            <Switch>
+                <Route path="/" exact component={SectionContainer} />
+                <Route path="/movie/:id" component={DetailContainer} />
+                <Route path="/test" WrappedComponent={withBoxoffice} />
+                <Redirect from="*" to="/" />
+            </Switch>
+            <Footer />
+        </>
+    </HashRouter>
 );
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,78 +3,86 @@ import { Link } from "react-router-dom";
 import Logo from "../../svg/Logo";
 
 class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      changeHeader: false, //모바일용으로 메뉴 바꿈
+    constructor(props) {
+        super(props);
+        this.state = {
+            changeHeader: false, //모바일용으로 메뉴 바꿈
+        };
+    }
+
+    //모바일용 메뉴
+    setMobileMwnu = () => {
+        this.setState({
+            changeHeader: !this.state.changeHeader,
+        });
     };
-  }
 
-  //모바일용 메뉴
-  setMobileMwnu = () => {
-    this.setState({
-      changeHeader: !this.state.changeHeader,
-    });
-  };
+    render() {
+        return (
+            <header
+                className={`header ${
+                    this.state.changeHeader ? "mobile-menu-header" : ""
+                }`}>
+                <div>
+                    <div className="header-left-box">
+                        <div className="header-mo-gnb">
+                            <ul>
+                                <input
+                                    type="checkbox"
+                                    onClick={this.setMobileMwnu}></input>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                        </div>
+                        {/* moblie ver gnb */}
+                        <Link to={"/"}>
+                            <div className="header-logo">
+                                <Logo />
+                            </div>
+                        </Link>
+                        <div className="header-gnb">
+                            <ul>
+                                <li>
+                                    <Link to={`/test`}>일별 랭킹</Link>
+                                </li>
 
-  render() {
-    return (
-      <header
-        className={`header ${
-          this.state.changeHeader ? "mobile-menu-header" : ""
-        }`}
-      >
-        <div>
-          <div className="header-left-box">
-            <div className="header-mo-gnb">
-              <ul>
-                <input type="checkbox" onClick={this.setMobileMwnu}></input>
-                <li></li>
-                <li></li>
-                <li></li>
-              </ul>
-            </div>
-            {/* moblie ver gnb */}
-            <Link to={"/"}>
-              <div className="header-logo">
-                <Logo />
-              </div>
-            </Link>
-            <div className="header-gnb">
-              <ul>
-                <li>일별 랭킹</li>
-                <li>주간 랭킹</li>
-                <li>주말 랭킹</li>
-              </ul>
-            </div>
-            {/* pc ver gnb */}
-          </div>
-          <div className="header-right-box">
-            <div className="header-search">
-              <span></span>
-              <input type="text" placeholder="Search" />
-            </div>
-            <div className="header-user">
-              <ul>
-                <li>회원가입</li>
-                <li>로그인</li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="addHeader">
-          <ul>
-            <li>로그인</li>
-            <li>회원가입</li>
-            <li>박스오피스</li>
-            <li>영화정보</li>
-            <li>영화사정보</li>
-            <li>영화인정보</li>
-          </ul>
-        </div>
-      </header>
-    );
-  }
+                                <li>
+                                    <Link to={`/test`}>주간 랭킹</Link>
+                                </li>
+                                <li>
+                                    <Link to={`/test`}>주말 랭킹</Link>
+                                </li>
+                            </ul>
+                        </div>
+                        {/* pc ver gnb */}
+                    </div>
+                    <div className="header-right-box">
+                        <div className="header-search">
+                            <span></span>
+                            <input type="text" placeholder="Search" />
+                        </div>
+                        <div className="header-user">
+                            <ul>
+                                <li>회원가입</li>
+                                <li>로그인</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="addHeader">
+                    <ul>
+                        <li>로그인</li>
+                        <li>회원가입</li>
+                        <li>박스오피스</li>
+                        <li>영화정보</li>
+                        <li>영화사정보</li>
+                        <li>영화인정보</li>
+                    </ul>
+                </div>
+            </header>
+        );
+    }
 }
 
 export default Header;
