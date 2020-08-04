175778402fe799668436d0eb140ee731326c374f
jhyooooon <jhyooooooon@gmail.com>
2020-08-04 14:30:13 +0900

    Fix header padding

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,7 @@ import React from "react";
 class Loading extends React.Component {
   render() {
     return (
-      <div class="loader loader--style3" title="2">
+      <div className="loader loader--style3" title="2">
         <svg
           version="1.1"
           id="loader-1"
