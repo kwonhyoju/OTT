78813087ee3656e12f8a9e8fa59889a6e64c125b
kwonhyoju <saung3754@gmail.com>
2020-09-02 21:03:55 +0900

    search page img 로직 수정, 버튼 디자인 수정, 간격 전체 조정, font-size 교체 ,color전체 교체, noimg 이미지 추가

--- src/components/section/SearchWrap.js
+++ src/components/section/SearchWrap.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import noimg from "../../img/noimg.png"
 
 /* 검색결과 영화 한개씩 띄우기 */
 const printData = (item, genreData, i) => {
+  console.log(":::img::::",item.poster_path);
+  let changeImg = item.poster_path===null?noimg:`https://image.tmdb.org/t/p/w342/${item.poster_path}`;
   return (
     <div key={i}>
       <div>
         <p className="title">{item.title}</p>
+        <div className="line"></div>
+        <div className="movie-image">
+          <img
+            src={changeImg}
+            alt="photo"
+          />
+        </div>
         <p className="genre">
           {genreData.map((genre) => {
             let genreName = "";
@@ -15,13 +25,6 @@ const printData = (item, genreData, i) => {
             return genreName;
           })}
         </p>
-        <div className="line"></div>
-        <div className="movie-image">
-          <img
-            src={`https://image.tmdb.org/t/p/w342/${item.poster_path}`}
-            alt="photo"
-          />
-        </div>
         <p className="subtext">{item.overview}</p>
         <Link to={`/movie/${item.id}`}>
           <div className="movie-btn">
