e5865061449f66effb93d02edfe4820c36ca48e0
jhyooooon <jhyooooooon@gmail.com>
2020-09-04 22:23:45 +0900

    Set centerMode false at mainRoll when window width less than 480px

--- src/components/section/SectionWrap.js
+++ src/components/section/SectionWrap.js
@@ -4,7 +4,7 @@ import Slider from "react-slick";
 import Poster from "../Poster";
 // import currentimg from "../../img/우리집.png";
 
-const mainRoll = (popData, genreData) => {
+const mainRoll = (popData, genreData, centerMode) => {
   const settings = {
     arrows: true,
     dots: false,
@@ -12,7 +12,7 @@ const mainRoll = (popData, genreData) => {
     autoplay: true,
     slidesToShow: 1,
     lazyLode: "ondemand",
-    // centerMode: true,
+    centerMode: centerMode,
     rows: 1,
     autoplaySpeed: 5000,
   };
@@ -51,24 +51,52 @@ const mainRoll = (popData, genreData) => {
   );
 };
 
-const SectionWrap = ({ popData, nowData, upcomeData, genreData }) => {
+class SectionWrap extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      centerMode: null,
+    };
+    this.handleResize = this.handleResize.bind(this);
+  }
 
-  return (
-    <section className="section-wrap">
-      <div className="section-main_roll">{mainRoll(popData, genreData)}</div>
-      {/* main-roll */}
-      <div className="section-current-movies">
-        <p>현재상영작</p>
-        <Poster data={nowData} genreData={genreData} diffWidth={false} />
-      </div>
-      {/* 현재 상영작 */}
-      <div className="section-schedule-movies">
-        <p>개봉예정작</p>
-        <Poster data={upcomeData} genreData={genreData} diffWidth={true} />
-      </div>
-      {/* 개봉 예정작 */}
-    </section>
-  );
-};
+  componentDidMount() {
+    this.handleResize();
+    window.addEventListener("resize", this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleResize);
+  }
+
+  handleResize() {
+    const mainWidth = document.getElementsByClassName("App")[0].clientWidth;
+    mainWidth < 480
+      ? this.setState({ centerMode: false })
+      : this.setState({ centerMode: true });
+  }
+  render() {
+    const { genreData, nowData, popData, upcomeData } = this.props;
+    const { centerMode } = this.state;
+    return (
+      <section className="section-wrap">
+        <div className="section-main_roll">
+          {mainRoll(popData, genreData, centerMode)}
+        </div>
+        {/* main-roll */}
+        <div className="section-current-movies">
+          <p>현재상영작</p>
+          <Poster data={nowData} genreData={genreData} diffWidth={false} />
+        </div>
+        {/* 현재 상영작 */}
+        <div className="section-schedule-movies">
+          <p>개봉예정작</p>
+          <Poster data={upcomeData} genreData={genreData} diffWidth={true} />
+        </div>
+        {/* 개봉 예정작 */}
+      </section>
+    );
+  }
+}
 
 export default SectionWrap;
